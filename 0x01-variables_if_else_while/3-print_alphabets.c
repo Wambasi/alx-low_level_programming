6c081526e29123588a88c0a674f895279c988353
Wambasi <wambasivin@gmail.com>
2023-06-16 07:03:40 -0700

    Prints lower and uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: 'Print alphabets in lower and uppercase'
+ * Return: always 0
+ */
+int main(void)
+
+{
+	int n = 97;
+	int n = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (n <= 90)
+	{
+		putchar (n);
+	}
+	putchar("\n");
+	return (0);
+}
+
