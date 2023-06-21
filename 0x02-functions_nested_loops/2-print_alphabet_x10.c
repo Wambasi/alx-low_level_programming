c5aef39e7ab190585fc52807796e5203478609ed
Wambasi <wambasivin@gmail.com>
2023-06-21 01:05:04 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
 	int i;
+	int j;
 
-	i = 0;
-
-	while (i < 10)
+	for (j = 1; i <= 10; i++)
 	{
-		ch 'a';
-		while (ch <= 'z')
+		for (j = 97; j <= 122; j++)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
