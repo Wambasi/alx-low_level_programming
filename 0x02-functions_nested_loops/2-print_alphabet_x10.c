d1070bf6d79320258141182f37119930efe1e99a
Wambasi <wambasivin@gmail.com>
2023-06-21 00:50:43 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times
- *  
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
@@ -11,11 +11,10 @@ void print_alphabet_x10(void)
 
 	for (j = 1; i <= 10; i++)
 	{
-
-		for (j = 97; i <= 122; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
-		_putchar ('\n');
- 	}
+		_putchar('\n');
+	}
 }
