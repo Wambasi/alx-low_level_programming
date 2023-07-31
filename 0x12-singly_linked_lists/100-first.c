decb37fba1529abced6a06cf4cd32effa2574c04
Wambasi <wambasivin@gmail.com>
2023-07-31 03:34:32 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 void first(void) __attribute__ ((constructor));
 
 /**
- *  * first - prints a sentence before the main
- *   * function is executed
- *    */
+ *first - prints a sentence before the main
+ *function is executed
+ */
 void first(void)
 {
 	 printf("You're beat! and yet, you must allow,\n");
