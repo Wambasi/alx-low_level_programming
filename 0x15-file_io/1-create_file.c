725bf30a22de359f047ef5b2d931566c7f7e637d
Wambasi <wambasivin@gmail.com>
2023-11-03 02:25:24 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * create_file -  a function that creates a file.
- * then copy content on it
- * @filename: filename to be created
+ * create_file -  a function that creates a file
+ * and copy content in it
+ * @filename: the filename to be created
  * @text_content: the content to be copied into the file
- * Return: 1 if successful, -1 if failed
+ * Return: 1 if successful or -1 if failed
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
 	int rwr;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -26,11 +25,12 @@ int create_file(const char *filename, char *text_content)
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
+
 	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
-	close (fd);
+	close(fd);
 
 	return (1);
 }
